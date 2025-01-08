2a45724788756447eda5c6f656f3fcc99d8b0346
Arthur Ramos <146880822+ArthurRamos26@users.noreply.github.com>
2025-01-08 16:48:45 -0400

    Add files via upload

---
+++ Seção_2/2-Soma3val.c
@@ -0,0 +1,15 @@
+//peça ao  usuário tres valores e imprima a soma deles 
+int main (){
+    int val1 , val2 , val3 , soma;
+    
+    printf("Digite um valor : ");
+    scanf("%d",&val1);
+    printf("Digite outro valor  : ");
+    scanf("%d",&val2);
+    printf("Digite o terceiro valor ");
+    scanf("%d",&val3);
+    soma = val1 + val2+ val3 ;
+    printf("o valor da soma e  %d", soma);
+    return 0; 
+
+}
