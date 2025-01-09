4c2202687e7b64cea381c5f41793f1c50e732a6a
Arthur Ramos <146880822+ArthurRamos26@users.noreply.github.com>
2025-01-09 11:54:45 -0400

    Update 1-5_multiplos_de_.c

--- Seção_4/1-5_multiplos_de_.c
+++ Seção_4/1-5_multiplos_de_.c
@@ -5,7 +5,7 @@ int main (){
     int mult = 0, n=1 ; 
     while(mult != 5){
        if(n % 3 == 0 ){ //checa se o resto da divisão por 3 é 0 sendo assim multiplo 
-        printf("%d E multiplo \n",n);
+        printf("%d E multiplo de 3 \n",n);
         mult++;
        }
         n++;
