5588f6601e45a1585921dd02c249ee7cf6d6a3dc
Arthur Ramos <arthurcorreiaufrr@gmail.com>
2025-02-07 01:04:38 -0400

    mudei os comentarios dos exercicios

--- Seção_12/2-fatorial.c
+++ Seção_12/2-fatorial.c
@@ -1,4 +1,4 @@
-// 2. Crie um programa que tenha uma função recursiva que calcule e retorne o fatorial de um número inteiro N.
+// 2. Crie um programa que tenha uma função recursiva que calcule e retorne o fatorial de um número inteiro N. dado a formula de fatoria fat = n*(n-1).......
 
 #include<stdio.h>
 int calc_fatorial(int n){
@@ -6,7 +6,7 @@ int calc_fatorial(int n){
         return 1;
     }
    
-    return n * (calc_fatorial(n-1)); //por se tratar de fatorial é a soma dele com o seu anterior 
+    return n * (calc_fatorial(n-1)); //por se tratar de fatorial é a soma dele com o seu anterior e aq é so -1 pois vai empilhando os valores apos cada execuçes ;
     
 }
 int main (){
